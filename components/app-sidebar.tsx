39c3378c57332686c346e683a5ae2a2ca20c6f0d
boloka02 <boloka02@gmail.com>
2025-08-03 11:20:05 +0800

    feat: updated sidebar components

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,8 +14,9 @@ import {
   SquareTerminal,
 } from "lucide-react";
 
-import { NavMain } from "@/components/nav-main";
-import { NavProjects } from "@/components/nav-projects";
+import { NavFinance } from "@/components/nav-finance";
+import { Navhr } from "@/components/nav-hr";
+import { Navemployee } from "@/components/nav-employee";
 import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
@@ -49,69 +50,71 @@ const data = {
       plan: "Free",
     },
   ],
-  navMain: [
+
+
+//.. Finance
+  navfinance: [
     {
-      title: "Playground",
+      title: "Dashboard",
       url: "#",
       icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
     },
+
+
     {
-      title: "Models",
+      title: "Payroll Management",
       url: "#",
       icon: Bot,
       items: [
         {
-          title: "Genesis",
+          title: "Process Payroll",
           url: "#",
         },
         {
-          title: "Explorer",
+          title: "View Salary Summary",
           url: "#",
         },
         {
-          title: "Quantum",
+          title: "Apply Deductions ",
           url: "#",
         },
       ],
     },
+    
     {
-      title: "Documentation",
+      title: "Tax Report",
       url: "#",
       icon: BookOpen,
       items: [
         {
-          title: "Introduction",
+          title: "Generate BIR Forms",
           url: "#",
         },
         {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
+          title: "View Tax History",
           url: "#",
         },
       ],
     },
+
+
+    {
+      title: "Financial Reports",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Employee Cost Summary",
+          url: "#",
+        },
+        {
+          title: "Monthly Expenses",
+          url: "#",
+        },
+      ],
+    },
+
+
     {
       title: "Settings",
       url: "#",
@@ -136,31 +139,239 @@ const data = {
       ],
     },
   ],
-  projects: [
+//.. End of Finance
+
+
+
+
+//.. Start Human Resource
+  navhr: [
     {
-      name: "Design Engineering",
+      title: "Dashboard",
       url: "#",
-      icon: Frame,
+      icon: SquareTerminal,
     },
+
     {
-      name: "Sales & Marketing",
+      title: "Employee Management",
       url: "#",
-      icon: PieChart,
+      icon: Bot,
+      items: [
+        {
+          title: "List of Employees",
+          url: "#",
+        },
+        {
+          title: "Add/Edit Employee Info",
+          url: "#",
+        },
+        {
+          title: "Employment Status History",
+          url: "#",
+        },
+      ],
     },
-    {
-      name: "Travel",
+
+
+      {
+      title: "Masterlist of Timesheet",
       url: "#",
-      icon: Map,
+      icon: SquareTerminal,
+    },
+    
+    {
+      title: "Leave Management",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Leave History",
+          url: "#",
+        },
+        {
+          title: "Approve/Decline Leave",
+          url: "#",
+        },
+      ],
+    },
+
+
+     {
+      title: "Performance Review",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Employee Evaluations",
+          url: "#",
+        },
+        {
+          title: "Review Templates",
+          url: "#",
+        },
+      ],
+    },
+
+
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Team",
+          url: "#",
+        },
+        {
+          title: "Billing",
+          url: "#",
+        },
+        {
+          title: "Limits",
+          url: "#",
+        },
+      ],
     },
   ],
+  //.. End of Human Resource
+
+
+
+  
+//.. Start Employee
+  navemployee: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: SquareTerminal,
+    },
+
+    {
+      title: "Request",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Leave Request",
+          url: "#",
+        },
+        {
+          title: "Change Schedule Request",
+          url: "#",
+        },
+        {
+          title: "Travel Request",
+          url: "#",
+        },
+      ],
+    },
+
+
+      {
+      title: "Update Log",
+      url: "#",
+      icon: SquareTerminal,
+    },
+    
+    {
+      title: "OT",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "OT Request Form",
+          url: "#",
+        },
+        {
+          title: "OT Status",
+          url: "#",
+        },
+      ],
+    },
+
+
+     {
+      title: "Payslip",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Monthly Payslip View",
+          url: "#",
+        },
+        {
+          title: "Download Payslip PDF",
+          url: "#",
+        },
+      ],
+    },
+
+
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Change password",
+          url: "#",
+        },
+        {
+          title: "Notification Preferences",
+          url: "#",
+        },
+      ],      
+    },
+
+    
+     {
+      title: "Attendance",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Daily Time Record",
+          url: "#",
+        },
+        {
+          title: "Log In/Out History",
+          url: "#",
+        },
+      ],
+    },
+
+     {
+      title: "Profile",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "View Personal Info",
+          url: "#",
+        },
+        {
+          title: "Update Contact Details",
+          url: "#",
+        },
+      ],
+    },
+
+  ],
+  //.. End of Employee
+
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <NavFinance items={data.navfinance} />
+        <Navhr items={data.navhr} />
+        <Navemployee items={data.navemployee} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
